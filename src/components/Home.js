21de1070fb41bd645f73ea682adc75a2cbfe40d8
mkellner43 <matthew.kellner@outlook.com>
2023-02-01 15:25:13 -0600

    updated img sizing

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,19 +10,19 @@ const Home = () => {
         <div className='game-selection'>
         <Link to="/easy">
           <h4 className='title'>Easy</h4>
-            <img className='small-img' src={require('../images/skiSlopes.jpeg')} alt="colorful where's waldo field of characters"/>
+            <img className='small-img home' src={require('../images/skiSlopes.jpeg')} alt="colorful where's waldo field of characters"/>
           </Link>
         </div>
         <div>
         <Link to="/medium">
           <h4 className='title'>Medium</h4>
-            <img className='small-img' src={require('../images/spaceStation.jpeg')} alt="colorful where's waldo field of characters"/>
+            <img className='small-img home' src={require('../images/spaceStation.jpeg')} alt="colorful where's waldo field of characters"/>
           </Link>
         </div>
         <div>
         <Link to="/hard">
           <h4 className='title'>Hard</h4>
-            <img className='small-img' src={require('../images/fruitLand.jpeg')} alt="colorful where's waldo field of characters"/>
+            <img className='small-img home' src={require('../images/fruitLand.jpeg')} alt="colorful where's waldo field of characters"/>
         </Link>
         </div>
       </main>
